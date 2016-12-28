1b9a0cb21b0ddeba797f00692dfbc93024a5a3c6
Madays <maday.choque@gmail.com>
2016-12-28 13:09:10 -0500

    agragando tabla

--- main.js
+++ main.js
@@ -5,14 +5,12 @@ function posNeg(){
     
     if (estado==2){
         if ((a>0 && b<0)||(a<0 && b>0)){
-        console.log("fb");
         document.getElementById("resultado").innerHTML="1"   ;
         }else{
             document.getElementById("resultado").innerHTML="0"   ;
         }
     }else if (estado==1){
         if (a<0 && b<0){
-            console.log("fb");
             document.getElementById("resultado").innerHTML="1"   ;
         }else{
             document.getElementById("resultado").innerHTML="0"   ;
